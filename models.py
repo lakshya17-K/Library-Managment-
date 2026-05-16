134258f4b3be5af57df37a36a2f15b684c241fcc
Lakshya Kumar Tripathi <lakshyakumartripathi@gmail.com>
2026-05-16 22:25:51 +0530

    Add genre attribute to Book class

--- models.py
+++ models.py
@@ -1,10 +1,17 @@
 class Book:
 
-    def __init__(self, book_id, title, author):
+    def __init__(
+        self,
+        book_id,
+        title,
+        author,
+        genre
+    ):
 
         self.id = book_id
         self.title = title
         self.author = author
+        self.genre = genre
 
         self.available = True
         self.borrowed_by = ""
@@ -16,6 +23,7 @@ class Book:
             "id": self.id,
             "title": self.title,
             "author": self.author,
+            "genre": self.genre,
             "available": self.available,
             "borrowed_by": self.borrowed_by,
             "borrow_date": self.borrow_date
